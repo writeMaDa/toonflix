748d1a2bcbfca6e1dcda67a2c30bead2cb74a7ea
Jaehong Lee <lljjhh117@naver.com>
2024-02-25 23:23:43 +0900

    4.2 setState Recap

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int counter = 0;
+  List<int> numbers = [];
+
   void onClicked() {
     setState(() {
-      //build 메서드를 다시 호출해서 화면을 새로고침해줌(바뀐 값으로)
-      counter = counter + 1; //하지만 굳이 setState안에서 할 필요는 없음. 안에서 하면 가독성이 좋을 뿐
+      numbers.add(numbers.length);
     });
   }
 
@@ -33,10 +33,7 @@ class _MyAppState extends State<MyApp> {
                 'Click Count',
                 style: TextStyle(fontSize: 30),
               ),
-              Text(
-                '$counter',
-                style: const TextStyle(fontSize: 30),
-              ),
+              for (var n in numbers) Text('$n'),
               IconButton(
                 onPressed: onClicked,
                 icon: const Icon(Icons.add_box_rounded),
