5929fa64a15ec7113e22de21db9aabf58b25281f
Jaehong Lee <lljjhh117@naver.com>
2024-03-21 15:30:14 +0900

    5.4 Code Challenge
    Make reset button
    Set times

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,6 +10,9 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   static const twentyFiveMinutes = 1500;
+  static const tenMinutes = 2;
+  static const fiftyMinutes = 3000;
+  int setSeconds = twentyFiveMinutes;
   int totalSeconds = twentyFiveMinutes;
   bool isRunning = false;
   int totalPomodoros = 0;
@@ -21,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
       setState(() {
         totalPomodoros = totalPomodoros + 1;
         isRunning = false;
-        totalSeconds = twentyFiveMinutes;
+        totalSeconds = setSeconds;
       });
       timer.cancel();
     } else {
@@ -45,6 +47,27 @@ class _HomeScreenState extends State<HomeScreen> {
     timer.cancel();
     setState(() {
       isRunning = false;
+    });
+  }
+
+  void resetTime() {
+    if (!isRunning) {
+      setState(() {
+        totalSeconds = setSeconds;
+        totalPomodoros = 0;
+      });
+    }
+  }
+
+  void setTime(int a) {
+    setState(() {
+      if (!isRunning && a == 1) {
+        setSeconds = tenMinutes;
+      } else if (!isRunning && a == 2) {
+        setSeconds = twentyFiveMinutes;
+      } else if (!isRunning && a == 3) {
+        setSeconds = fiftyMinutes;
+      }
     });
   }
 
@@ -76,13 +99,112 @@ class _HomeScreenState extends State<HomeScreen> {
           Flexible(
             flex: 3,
             child: Center(
-              child: IconButton(
-                iconSize: 120,
-                color: Theme.of(context).cardColor,
-                onPressed: isRunning ? onPausePressed : onStartPressed,
-                icon: Icon(isRunning
-                    ? Icons.pause_circle_outline
-                    : Icons.play_circle_outline),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          setTime(1);
+                        },
+                        child: Container(
+                          height: 60,
+                          width: 110,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).cardColor,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '10:00',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          setTime(2);
+                        },
+                        child: Container(
+                          height: 60,
+                          width: 110,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).cardColor,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '25:00',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          setTime(3);
+                        },
+                        child: Container(
+                          height: 60,
+                          width: 110,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).cardColor,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '50:00',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        iconSize: 120,
+                        color: Theme.of(context).cardColor,
+                        onPressed: isRunning ? onPausePressed : onStartPressed,
+                        icon: Icon(isRunning
+                            ? Icons.pause_circle_outline
+                            : Icons.play_circle_outline),
+                      ),
+                      IconButton(
+                        iconSize: 120,
+                        color: Theme.of(context).cardColor,
+                        onPressed: resetTime,
+                        icon: const Icon(Icons.restore_outlined),
+                      )
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
